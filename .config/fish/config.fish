ae6e808548dd3050835a1308a11f4eab84d6d510
p2411kh <2411khlol@gmail.com>
2026-07-23 09:32:21 +0200

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+        fastfetch
+        alias ls='lsd'
+        alias l='ls -l'
+        alias la='ls -a'
+        alias lla='ls -la'
+        alias lt='ls --tree'
+        alias cat='bat'
+end
+
+
+set -U fish_greeting ""
