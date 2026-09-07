32f988bd8069b33bac8677f245aff34986bbf454
p2411kh <2411khlol@gmail.com>
2026-09-07 15:19:28 +0200

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+   fastfetch
+   set -g fish_greeting ""
+   alias ls='lsd'
+   alias l='ls -l'
+   alias ls='ls -a'
+   alias lla='ls =la'
+   alias lt='ls --tree'
+
+end
